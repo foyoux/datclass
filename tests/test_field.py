dc0243f8ccb0570643bd5c9e71001867a97d23e4
foyou <yimi.0822@qq.com>
2023-02-20 10:08:59 +0800

    add test_filed

---
+++ tests/test_field.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field, is_dataclass
+from typing import List, Dict
+
+from datclass import DatClass
+
+
+@dataclass
+class Person(DatClass):
+    name: str = None
+    friends: List[Person] = field(default_factory=list)
+
+
+def test_field_default_factory_list():
+    dat = {
+        'name': 'foyou',
+    }
+    p = Person(**dat)
+    assert p.name == 'foyou'
+    assert isinstance(p.friends, list) and len(p.friends) == 0
+
+
+@dataclass
+class User(DatClass):
+    name: str
+    info: Dict = field(default_factory=dict)
+
+
+def test_field_default_factory_dict():
+    dat = {
+        'name': 'foyou',
+    }
+    user = User(**dat)
+    assert user.name == 'foyou'
+    assert isinstance(user.info, dict) and len(user.info) == 0
+
+
+@dataclass()
+class Student(DatClass):
+    name: str = 'foyou'
+
+
+def test_is_dataclass():
+    s = Student(name='hello')
+    assert is_dataclass(s)
+    assert isinstance(s, Student)
+    assert s.name == 'hello'
