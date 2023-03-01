2cb267513b12c19a0be0176d73995819e7ea1abf
foyou <yimi.0822@qq.com>
2023-03-01 14:42:00 +0800

    add test_generate_cls

---
+++ tests/test_generate_cls.py
@@ -0,0 +1,18 @@
+import datclass
+
+
+def test_generate_cls(data):
+    codes = datclass.gen_datclass(data, recursive=True)
+    dat = '\n'.join(datclass.imports.to_list() + codes + [
+        'obj = Object(**data)',
+        'assert obj.update_id == 1',
+        'assert isinstance(obj.message, Message)',
+        'assert isinstance(obj.message.a_2U3sadsb, A2U3Sadsb)',
+        'assert isinstance(obj.message.video, Video)',
+        'assert isinstance(obj.message.caption_entities, list)',
+        'for c in obj.message.caption_entities:',
+        '    assert isinstance(c, CaptionEntities)',
+        'assert isinstance(obj.message.forward_from, ForwardFrom)',
+    ])
+    ns = {'data': data}
+    exec(dat, ns)
