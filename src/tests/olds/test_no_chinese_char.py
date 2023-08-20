3c0251c5ae8f550e1319f8643d60e95232238f18
foyou <yimi.0822@qq.com>
2023-08-20 13:52:07 +0800

    update tests

--- src/tests/olds/test_no_chinese_char.py
+++ src/tests/olds/test_no_chinese_char.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 
 def test_readme_md():
-    readme = Path(os.path.dirname(__file__)).parent.parent.joinpath('README.md').read_text(encoding='utf8')
+    readme = Path(os.path.dirname(__file__)).parent.parent.parent.joinpath('README.md').read_text(encoding='utf8')
     results = re.findall(r'[\u4e00-\u9fff]+', readme)
     assert len(results) == 0, f'Chinese characters in README.md: {results}'
